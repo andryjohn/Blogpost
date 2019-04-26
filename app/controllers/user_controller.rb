113564f6c909dea2fea4744dfd0104ed2ef4c8bd
rajohnson andry <Andry12@hotmail.fr>
2019-04-26 17:05:53 +0200

    edition du blog , adding Bootstrap, routes, link

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,19 +7,13 @@ before_action :set_user, only: [:show, :edit, :update, :destroy]
   def show
   end
 
-  def new
-  end
-
   def edit
   end
 
-  def create
-  end
-
-  def update
-  end
 
   def destroy
+    @user.destroy
+    redirect_to root_path
   end
 
   private 
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-	root to: 'posts#new'
-
-  get 'user/index'
-  get 'user/show'
-  get 'user/new'
-  get 'user/edit'
-  get 'user/create'
-  get 'user/update'
-  get 'user/destroy'
-  devise_for :users
-  resources :posts
+	root to: 'pages#home'
+ devise_for :users
+ resources :users 
+ resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
